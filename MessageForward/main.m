55070b0c2de468084995af7caee4e4ff6154504a
MiaoChao <878310229@qq.com>
2015-12-20 13:53:27 +0800

    Initial Commit

---
+++ MessageForward/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MessageForward
+//
+//  Created by MiaoChao on 15/12/20.
+//  Copyright © 2015年 MiaoChao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
